f7d3cefb38d3a3ac7ff29dc0624d3180a7c90226
Nobuhiro Nikushi <deneb.ge@gmail.com>
2013-12-19 20:07:36 +0900

    Initial

---
+++ lib/fluent/plugin/derive.rb
@@ -0,0 +1,9 @@
+require "fluent/plugin/derive/version"
+
+module Fluent
+  module Plugin
+    module Derive
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/fluent/plugin/derive/version.rb
@@ -0,0 +1,7 @@
+module Fluent
+  module Plugin
+    module Derive
+      VERSION = "0.0.1"
+    end
+  end
+end
